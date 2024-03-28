a5072f620e76ef33e41c29a298f4d4372d144125
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-28 10:57:56 +0000

    migrated new models in polls.models  to the db

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_lenght=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
 
