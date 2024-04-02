e0ae71b0e0f5e1ff8b88b1492921cb61476df032
Tom Fyfe <tomfyfe85@gmail.com>
2024-04-02 21:23:34 +0100

    tutorial part 2 'playing with the api' complete

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,9 +11,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=9)
 
 
 class Choice(models.Model):
