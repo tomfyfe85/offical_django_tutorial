a3d34cbb61c15b7f7c69eed52f23d11744f49ebf
Tom Fyfe <tomfyfe85@gmail.com>
2024-04-03 23:57:53 +0100

    up to part 3, a shortcut: render()

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    output = ", ".join([q.question_text for q in latest_question_list])
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    print(request)
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
