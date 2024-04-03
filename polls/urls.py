42c7b58821ed904107abe82c42ada47e2137f9a5
Tom Fyfe <tomfyfe85@gmail.com>
2024-04-03 22:29:15 +0100

    part 3 - upto Write views that actually do something

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # ex: /polls/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+    # ex: /polls/5/vote/
 ]
