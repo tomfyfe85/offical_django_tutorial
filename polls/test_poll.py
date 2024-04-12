cf5da0d421b876d0e28bc3a7234bd2feb79d8b22
Tom Fyfe <tomfyfe85@gmail.com>
2024-04-12 18:54:13 +0100

    test_was_published_recently_with_future_question() not yet passing

--- polls/test_poll.py
+++ polls/test_poll.py
@@ -4,12 +4,17 @@ from .models import Question
 
 # Create your tests here.
 import pytest
+# class QuestionModelTests():
 
-"""
-was_published_recently() returns False for questions whose pub_date
-is in the future
-"""
+def test_was_published_recently_with_future_question():
 
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        assert future_question.was_published_recently == False
 
 """
 was_published_recently() returns True for questions whose pub_date
