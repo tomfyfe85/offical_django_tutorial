d4aea0436d8ddd37eba1f2c9faee7acb72dfd8d9
Tom Fyfe <tomfyfe85@gmail.com>
2024-04-12 18:19:41 +0100

    added pytest to the project

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
 
 # Create your tests here.
+"""
+was_published_recently() returns False for questions whose pub_date
+is older than 1 day.
+"""
+
+
+
+"""
+was_published_recently() returns True for questions whose pub_date
+is within the last day.
+"""
--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 from .models import Question, Choice
@@ -15,7 +16,9 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date_lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
@@ -44,4 +47,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-    return HttpResponseRedirect(reverse("polls:results", args=(question.id, )))
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
