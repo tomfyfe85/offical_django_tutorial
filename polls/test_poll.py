b0fb2e303ceec0996d15f12e9a90bb80295d3d83
Tom Fyfe <tomfyfe85@gmail.com>
2024-04-17 23:18:19 +0100

    part 5- up to test a view

--- polls/test_poll.py
+++ polls/test_poll.py
@@ -13,10 +13,28 @@ def test_was_published_recently_with_future_question():
         is in the future
         """
         time = timezone.now() + datetime.timedelta(days=30)
+        
+
         future_question = Question(pub_date=time)
-        assert future_question.was_published_recently == False
+        print(future_question)
+        assert future_question.was_published_recently() == False
 
 """
 was_published_recently() returns True for questions whose pub_date
 is within the last day.
 """
+
+def test_was_published_recently_with_an_old_question():
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        assert old_question.was_published_recently() == False
+
+"""
+was_published_recently() returns True for questions whose pub_date
+is within the last day.
+"""
+
+def test_was_published_recently_with_recent_question():
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        assert recent_question.was_published_recently() == True
